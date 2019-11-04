7371cd09c1a55d511e98b75843422559e722e4d0
bchuup <bchuup@gmail.com>
2019-11-04 01:34:19 -0500

    refactor initialization

--- src/contextProviders/FacebookSdkProvider.tsx
+++ src/contextProviders/FacebookSdkProvider.tsx
@@ -3,38 +3,36 @@ import React, { useEffect, FunctionComponent } from 'react';
 const MyFacebookContext = React.createContext<any>(null);
 
 declare global {
-  interface Window { fbAsyncInit: any, FB: any }
+  interface Window { fbAsyncInit: any, FB: fb.FacebookStatic }
 }
 
-// window.fbAsyncInit = window.fbAsyncInit || {};
-// window.FB = window.FB || {};
-
-// interface Window {
-//   fbAsyncInit: () => any;
-//   FB: () => any;
-// }
-
 const FacebookSdkProvider: FunctionComponent = ({ children }) => {
+
   useEffect(() => {
-    (async () => {
-      window.FB.init({
-        appId: process.env.REACT_APP_FB_APP_ID,
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v4.0'
-      });
-      window.FB.login(function (response: any) {
-        if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
-          console.log('authResponse', response.authResponse);
-          window.FB.getLoginStatus(function (response: any) {
-            console.log('getLoginStatus', response);
-          });
-        } else {
-          console.log('User cancelled login or did not fully authorize.');
-        }
-      });
-    })();
+    window.FB.getLoginStatus((response: fb.StatusResponse) => {
+      console.log('response', response);
+    });
+    // const fbAsyncInit = async () => {
+    //   window.FB.init({
+    //     appId: process.env.REACT_APP_FB_APP_ID || '',
+    //     autoLogAppEvents: true,
+    //     version: 'v4.0',
+    //   })
+      // window.FB.login(function (response: fb.StatusResponse) {
+      //   if (response.authResponse) {
+      //     console.log('Welcome!  Fetching your information.... ');
+      //     console.log('authResponse', response.authResponse);
+      //     window.FB.getLoginStatus(function (response: fb.StatusResponse) {
+      //       console.log('getLoginStatus', response);
+      //     });
+      //   } else {
+      //     console.log('User cancelled login or did not fully authorize.');
+      //   }
+      // });
+    // }
+    // fbAsyncInit().then(() => {
+    //   console.log(window);
+    // })
   });
   return (
     <MyFacebookContext.Provider value={{test: 'hello'}}>
