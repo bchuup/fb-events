88fd53f2e91bf9c00b5828891e3b3b9b3905271a
bchuup <bchuup@gmail.com>
2019-11-09 14:53:02 -0500

    introduced login button and icons

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,13 +1,24 @@
 import React, { FunctionComponent, useContext } from 'react';
-import { Button } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { MyFacebookContext } from '../../contextProviders/FacebookSdkProvider';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import styled from 'styled-components';
+
+const LogoutContainer = styled.div`
+  padding: 1em;
+  position: fixed;
+  top: 0;
+  right: 0;
+`
 
 const Home: FunctionComponent = () => {
   const { logout } = useContext(MyFacebookContext);
   return (
-    <div>
-      <Button onClick={logout} color="primary">logout</Button>
-    </div>
+    <LogoutContainer>
+      <IconButton onClick={logout} color="primary">
+        <ExitToAppIcon/>
+      </IconButton>
+    </LogoutContainer>
   )
 };
 
